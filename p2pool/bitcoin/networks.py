caa27cd54f367c209a583c54e534db4e53c5c2c7
James Holodnak <jamesholodnak@gmail.com>
2014-02-15 16:18:04 -0500

    Added general flappycoin parameters

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -135,6 +135,27 @@ nets = dict(
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=1e8,
     ),
+    flappycoin=math.Object(
+        P2P_PREFIX='c1c1c1c1'.decode('hex'),
+        P2P_PORT=11556,
+        ADDRESS_VERSION=35,
+        RPC_PORT=11555,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'flappycoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 10000*100000000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=60,
+        SYMBOL='FLAP',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'FlappyCoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Flappycoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.flappycoin'), 'flappycoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://flapplorer.flappycoin.info/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://flapplorer.flappycoin.info/address/',
+        TX_EXPLORER_URL_PREFIX='http://flapplorer.flappycoin.info/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=0.03e8,
+    ),
     dogecoin=math.Object(
         P2P_PREFIX='c0c0c0c0'.decode('hex'),
         P2P_PORT=22556,
